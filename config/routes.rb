c92619dd3b14b3ef75b0aa63e495c14cc49ef5a4
idakroniczaja <so.kida@hotmail.com>
2022-08-05 18:23:38 -0400

    logging in form

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,11 @@ Rails.application.routes.draw do
 
   get 'sign_up', to: 'registrations#new'
   post 'sign_up', to: 'registrations#create'
+  delete 'logout', to: 'sessions#destroy'
+
+  get 'sign_in', to: 'sessions#new'
+  post 'sign_in', to: 'sessions#create'
+
 
   root to:"main#index"
 

c82ad3c777856a1a1308b032ee211cb48b67734d
idakroniczaja <so.kida@hotmail.com>
2022-08-17 21:01:43 -0400

    twitter account model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
     end
     
     def create
-        user = User.find_by(email: params[:email])
-        if user.present? && user.authenticate(params:[:password])
-            session[:user_id] = user.id
+        @user = User.find_by(email: params[:email])
+        if @user.present? && @user.authenticate(params[:password])
+            session[:user_id] = @user.id
             redirect_to root_path, notice: 'Logged in successfully'
         else 
             redirect_to sign_up_path, alert: 'We couldnt find your account, please sign up.'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+    has_many :twitter_accounts
+    
     has_secure_password
 
     validates :email, presence: true, format: {with: /\b[A-Z0-9._%a-z\-]+@(?:[A-Z0-9a-z\-]+\.)+[A-Za-z]{2,4}\z/m, message: "Please enter valid email address."}
